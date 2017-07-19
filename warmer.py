8aa0a5ff069426ea62cc2cb6fc1fd4b38e2c85ed
Jared Grise <a30095180@mcla.edu>
2017-07-19 02:07:53 +0000

    adding Warmer

---
+++ warmer.py
@@ -0,0 +1,16 @@
+print 'Jared Grise'
+print 'Assingment 3 -- Warmer'
+print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+print ' ' 
+
+
+file = open('temps.txt','r')
+firsttemps = []
+for line in file:
+    words = line.split(" ")
+    for word in words:
+        
+print "During the first 81 years, the average deviation from the temperature anomoly is "
+print "During the first 81 years, %int has a positive temperature anomoly" %
+print "During the last 35 years, the average deviation from the temperature anomoly is "
+print "During the last 35 years, %int had a positive temperature anomoly" %
